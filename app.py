3b29895d897d2b5f047e70037118809a6888a76a
Dhwani Teli <dteli@umass.edu>
2025-10-03 00:01:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -53,8 +53,7 @@ st.success(f"Loaded dataset from {src}")
 # Context / Objective / About the Data (brief)
 # ---------------------------------------------------------
 st.header("Context")
-st.markdown("Insurance companies invest a lot of time and money into optimizing their pricing and accurately estimating the likelihood that customers will make a claim. In many countries insurance it is a legal requirement to have car insurance in order to drive a vehicle on public roads, so the market is very large!.
-Knowing all of this, On the Road car insurance have requested your services in building a model to predict whether a customer will make a claim on their insurance during the policy period. As they have very little expertise and infrastructure for deploying and monitoring machine learning models, they've asked you to identify the single feature that results in the best performing model, as measured by accuracy, so they can start with a simple model in production.")
+st.markdown("Insurance companies invest a lot of time and money into optimizing their pricing and accurately estimating the likelihood that customers will make a claim. In many countries insurance it is a legal requirement to have car insurance in order to drive a vehicle on public roads, so the market is very large!. Knowing all of this, On the Road car insurance have requested your services in building a model to predict whether a customer will make a claim on their insurance during the policy period. As they have very little expertise and infrastructure for deploying and monitoring machine learning models, they've asked you to identify the single feature that results in the best performing model, as measured by accuracy, so they can start with a simple model in production.")
 
 st.header("Objective")
 st.markdown("Identify the single feature of the data that is the best predictor of whether a customer will put in a claim.")
